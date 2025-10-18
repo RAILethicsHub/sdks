17f35a735342fbf9322d6e394a30027c92e1d97d
SUMIT VERMA <sumitverma9628@gmail.com>
2025-10-18 21:01:31 +0530

    push for release v1.0.0

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/RAILethicsHub/sdks/python",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests", "tests.*", "examples", "*.tests", "*.tests.*"]),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
